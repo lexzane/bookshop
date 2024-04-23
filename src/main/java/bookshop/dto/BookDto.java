728591b5966cde4c2afba92c3541396fc6c74cd7
Alex Zavodov <96802636+lexzane@users.noreply.github.com>
2024-04-23 04:38:39 +0300

    fix [springboot]: rewrite isbn validation

--- src/main/java/bookshop/dto/BookDto.java
+++ src/main/java/bookshop/dto/BookDto.java
@@ -2,11 +2,11 @@ package bookshop.dto;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.Size;
 import java.math.BigDecimal;
 import lombok.Data;
+import org.hibernate.validator.constraints.ISBN;
 
 @Data
 public class BookDto {
@@ -20,8 +20,7 @@ public class BookDto {
     @Size(max = 255)
     private String author;
     @NotBlank
-    @Size(max = 255)
-    @Pattern(regexp = "^\\d+(?:-?\\d+)*$", message = "must match digits or kebab case digits")
+    @ISBN
     private String isbn;
     @NotNull
     @Positive
