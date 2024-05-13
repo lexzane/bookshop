293f594df2fb49085ecb2e26d90c58196df57e3f
Alex Zavodov <96802636+lexzane@users.noreply.github.com>
2024-05-13 23:17:46 +0300

    feat [springboot]: add composite key to users_roles liquibase ddl; fix security permitions

--- src/main/java/bookshop/config/SecurityConfig.java
+++ src/main/java/bookshop/config/SecurityConfig.java
@@ -32,7 +32,8 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(
                         auth -> auth
-                                .requestMatchers("/api/auth/**", "/error", "/swagger-ui/**")
+                                .requestMatchers("/api/auth/**", "/error",
+                                        "/swagger-ui/**", "/v3/api-docs/**")
                                 .permitAll()
                                 .anyRequest()
                                 .authenticated()
